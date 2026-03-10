1d2fdbfb6290bbc8beee812174fdf7e47b272ecf
geraldo <contact@lafabriqueducode.fr>
2026-03-10 17:52:32 +0100

    change

--- config/database.js
+++ config/database.js
@@ -30,12 +30,20 @@ module.exports = ({ env }) => {
     },
     postgres: {
       connection: {
-        host: env("PGHOST", "localhost"),
-        port: env.int("PGPORT", 5432),
-        database: env("PGDATABASE", "railway"),
-        user: env("PGUSER", "postgres"),
-        password: env("PGPASSWORD", ""),
-        ssl: false,
+        connectionString: env("DATABASE_URL"),
+        host: env("DATABASE_HOST", "localhost"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "strapi"),
+        user: env("DATABASE_USERNAME", "strapi"),
+        password: env("DATABASE_PASSWORD", "strapi"),
+        ssl: env.bool("DATABASE_SSL", false)
+          ? {
+              rejectUnauthorized: env.bool(
+                "DATABASE_SSL_REJECT_UNAUTHORIZED",
+                false,
+              ),
+            }
+          : false,
         schema: env("DATABASE_SCHEMA", "public"),
       },
       pool: {
