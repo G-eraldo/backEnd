78495e1e76a0764bc7e414dae3244b104474d58c
geraldo <contact@lafabriqueducode.fr>
2026-03-10 19:09:04 +0100

    change

--- config/database.js
+++ config/database.js
@@ -1,46 +1,18 @@
 const path = require("path");
 
-module.exports = ({ env }) => {
-  const client = env("DATABASE_CLIENT", "sqlite");
-
-  const connections = {
-    sqlite: {
-      connection: {
-        filename: path.join(
-          __dirname,
-          "..",
-          env("DATABASE_FILENAME", ".tmp/data.db"),
-        ),
-      },
-      useNullAsDefault: true,
-    },
-    postgres: {
-      connection: {
-        // On essaie d'abord l'URL complète, sinon on prend les variables une par une
-        connectionString: env("DATABASE_URL"),
-        host: env("PGHOST", env("DATABASE_HOST")),
-        port: env.int("PGPORT", env.int("DATABASE_PORT", 5432)),
-        database: env("PGDATABASE", env("DATABASE_NAME")),
-        user: env("PGUSER", env("DATABASE_USERNAME")),
-        password: env("PGPASSWORD", env("DATABASE_PASSWORD")),
-        // Très important pour Railway :
-        ssl: env.bool("DATABASE_SSL", true)
-          ? { rejectUnauthorized: false }
-          : false,
-        schema: env("DATABASE_SCHEMA", "public"),
-      },
-      pool: {
-        min: env.int("DATABASE_POOL_MIN", 2),
-        max: env.int("DATABASE_POOL_MAX", 10),
-      },
-    },
-  };
-
-  return {
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
     connection: {
-      client,
-      ...connections[client],
-      acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
+      connectionString: env("DATABASE_URL"),
+      ssl: env.bool("DATABASE_SSL", true)
+        ? { rejectUnauthorized: false }
+        : false,
     },
-  };
-};
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    acquireConnectionTimeout: 60000,
+  },
+});
