83f713cb229257587e20a9fb33470054277c5d5d
geraldo <contact@lafabriqueducode.fr>
2026-03-24 18:40:00 +0100

    Ajout resend

--- config/plugins.js
+++ config/plugins.js
@@ -16,5 +16,17 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  email: {
+    config: {
+      provider: "strapi-provider-email-resend",
+      providerOptions: {
+        apiKey: env("RESEND_API_KEY"), // Required
+      },
+      settings: {
+        defaultFrom: "contact@lafabriqueducode.fr",
+        defaultReplyTo: "contact@lafabriqueducode.fr",
+      },
+    },
+  },
   // ... Éventuelles autres clefs
 });
